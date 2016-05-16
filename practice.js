65f45964b03aab6256f236b195d23e1fb22696b5
CJ Powers <cjpwrs@gmail.com>
2016-05-16 12:03:36 -0600

    first half done

--- practice.js
+++ practice.js
@@ -4,7 +4,10 @@
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
   //Code here
-
+var me = {
+  name: "CJ Powers", 
+  age: 24
+}
 
 
 
@@ -16,17 +19,28 @@
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
   //Code here
+var favoriteThings = {
+  band: "Yellow Card",
+  food: "Pizza",
+  person: "Tiarra",
+  book: "Harry Potter",
+  movie: "Tommy Boy",
+  holiday: "Christmas"
+}
 
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
   //Code here
+favoriteThings.car = "Mustang";
+favoriteThings.brand = "Nike";
 
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
 
   //Code here
-
+favoriteThings.food = "Lettuce";
+favoriteThings.book = "50 Shades of Gray";
 
 
 
@@ -43,15 +57,20 @@ Using dot notation, add another key (or property) to your backPack object
 that is named color, with the value being the color of your backpack. */
 
   //Code here
+var backPack = {};
+var item = "firstPocket";
 
+backPack[item] = "chapstick";
+backPack.color = "gray";
 //After you do the above, alert your entire backPack object.
 
   //Code here
+ // alert(backPack);
 
 /*You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. */
-
+console.log(backPack);
   //Code here
 
 
@@ -65,10 +84,22 @@ Instead, console.log your whole backPack object and then check out the console. 
 //Create an 'alsoMe' object with the following properties name, age, height, gender, married, eyeColor, hairColor. Fill those properties in with the appropriate values.
 
   //Code Here
+var alsoMe = {
+  name: "CJ",
+  age: 24,
+  height: "5'8''",
+  gender: "Male",
+  married: true,
+  eyeColor: "blue",
+  hairColor: "brown"
+}
 
 //Now, loop through your object and alert every value. *Tyler --> 24 --> 6'0 --> Male, etc etc
 
   //Code Here
+  /*for(var prop in alsoMe){
+    alert(alsoMe[prop]);
+  }*/
 
 
 
@@ -81,11 +112,20 @@ Instead, console.log your whole backPack object and then check out the console. 
 //Create an Object called 'album' with 5 keys named different song titles that you make up, with the values being the length of each song.
 
   //Code Here
+var album = {
+  one: 3,
+  two: 3,
+  three: 5,
+  four: 4,
+  five: 4
+}
 
 //Now, loop through your album object alerting every song title individually.
 
   //Code Here
-
+/*for(var prop in album){
+  alert(prop);
+}*/
 
 
 
@@ -97,10 +137,22 @@ Instead, console.log your whole backPack object and then check out the console. 
 //Create an object called states that has 5 US states as properties with the values being their population (doesn't have to be accurate).
 
   //Code Here
+var states = {
+  Washington: 444444,
+  Oregon: 54,
+  California: 6544335,
+  Idaho: 4343432,
+  Utah: 435643
+}
 
 //Now, loop through your states object and if the states population is greater than 30K, alert that state.
 
   //Code Here
+for(var prop in states){
+  if(states[prop] > 30000){
+    alert(prop);
+  }
+}
 
 
 
